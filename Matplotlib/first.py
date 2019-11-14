882a2632ea0f62f447c01a517712b03d318ceb75
strongestavenger <mikemutoro@gmail.com>
2019-11-14 08:04:14 +0300

    implement OOP with patplotlib

--- Matplotlib/first.py
+++ Matplotlib/first.py
@@ -28,4 +28,26 @@ plt.plot(y, x, 'g*-');
 
 """
 Object Oriented Method
+    :Create empty figure and add a set of axes to 
+    it. > fig = plt.figure()
 """
+
+fig = plt.figure()
+# axes1 = fig.add_axes([left, bottom, width, height])
+axes1 = fig.add_axes([0.1, 0.1, 0.8, 0.8]) # main axes
+axes2 = fig.add_axes([0.2, 0.5, 0.4, 0.3]) # inset axes
+# 20% from the left, 50% from bottom, 40% of width, 30% of total height
+
+# Larger Figure Axes 1
+axes1.plot(x, y, 'b')
+axes1.set_xlabel('X_label_axes2')
+axes1.set_ylabel('Y_label_axes2')
+axes1.set_title('Axes 2 Title')
+plt.show()
+
+# Insert Figure Axes 2
+axes2.plot(y, x, 'r')
+axes2.set_xlabel('X_label_axes2')
+axes2.set_ylabel('Y_label_axes2')
+axes2.set_title('Axes 2 Title')
+# plt.show()
